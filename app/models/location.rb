456d3446b5a4243b261ce1d45be4f30c5404243b
github username <github email address>
2017-12-05 16:47:54 -0500

    made some methods in workout and location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,30 @@
 class Location < ActiveRecord::Base
   has_many :workouts
   has_many :users, through: :workouts
-  # has_many :playlists, through: :workouts
+  has_many :playlists, through: :workouts
+
+  def self.find_clean
+    self.all.select do |location|
+      location.cleanliness >= 7
+    end.map { |clean| clean.name  }
+  end
+
+  def self.find_dirty
+    self.all.select do |location|
+      location.cleanliness < 7
+    end.map { |dirty| dirty.name  }
+  end
+
+  def self.find_empty
+    self.all.select do |location|
+      location.empty?
+    end.map { |empty| empty.name  }
+  end
+
+  def self.find_crowded
+    self.all.select do |location|
+      !location.empty?
+    end.map { |crowded| crowded.name  }
+  end
+
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,18 @@ class Workout < ActiveRecord::Base
   belongs_to :user
   belongs_to :location
   belongs_to :playlist
+
+  def self.longest
+    self.find_by(duration_mins: (self.maximum("duration_mins"))).description
+  end
+
+  def self.highest_rated
+    self.find_by(rating: (self.maximum("rating"))).description
+  end
+
+  def self.most_cals_burned
+    self.find_by(calories_burned: (self.maximum("calories_burned"))).description
+  end
+
+
 end
