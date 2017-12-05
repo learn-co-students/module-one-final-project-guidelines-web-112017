3029fe40949d0265164b4aba2557d07a0678a4ac
iamm28 <alisonmackay246@gmail.com>
2017-12-05 16:02:08 -0500

    user and playlist methods started

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,25 @@
 class Playlist < ActiveRecord::Base
   has_many :workouts
   has_many :users, through: :workouts
-  # has_many :locations, through: workouts:
+  has_many :locations, through: :workouts
+
+  def self.most_played
+    hash = Hash.new(0)
+    Workout.all.map do |w|
+      hash[w.playlist] += 1
+    end
+    hash.max_by{|k,v| v}.first.name 
+  end
+
+  def self.best_rated
+    self.find_by(rating: (self.maximum("rating"))).name
+  end
+
+  def self.longest_playlist
+    self.find_by(num_songs: (self.maximum("num_songs"))).name
+  end
+
+  def self.genre_count(genre)
+    self.where(genre: genre).length
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,29 @@ class User < ActiveRecord::Base
   has_many :workouts
   has_many :playlists, through: :workouts
   has_many :locations, through: :workouts
+
+  def add_workout(workout_inst)
+    self.workouts << workout_inst
+  end
+
+  def find_pool_workouts
+    self.workouts.location == "pool" || "Pool" #very specific location needed for this to work
+  end
+
+  def find_lifting_workouts
+    self.workouts.location == "weights" || "Weights" #very specific location needed for this to work
+  end
+
+  def find_last_workout
+    self.workouts.last #make by time and use timestamps
+  end
+
+  #def num_workouts_last_week
+  #end
+
+  def favorite_playlist
+    max_rating = self.playlists.maximum("rating")
+    self.playlists.find_by(rating: max_rating).name
+  end
+
 end
- 
